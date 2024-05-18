1ed2af78a5fa65cecf3149b83ffa1a42b6d9eb45
PrasannaNavale91 <125890869+PrasannaNavale91@users.noreply.github.com>
2024-05-18 11:14:04 +0530

    changes done

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const dbConnection = ()=>{
     mongoose.connect(process.env.MONGO_URI, {
-        dbName: 'MERN_STACK_JOB'
+        dbName: 'MERN_STACK_JOB',
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 30000,
     }).then(()=>{
         console.log("Connected to database!")
     }).catch((err)=>{
